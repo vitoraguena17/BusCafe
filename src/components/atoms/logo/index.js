10247590edbe4d9e4d74404109fd7a7ac8e63552
Vitor Aguena <144974439+vitoraguena17@users.noreply.github.com>
2025-01-08 20:53:30 -0300

    Update logo

--- src/components/atoms/logo/index.js
+++ src/components/atoms/logo/index.js
@@ -1,16 +1,25 @@
 import logo from '../../../images/logo/logo.png';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom'; 
 
 const LogoImg = styled.img`
     width: 200px;
+    cursor: pointer;
 `;
 
-function Logo({ className }) { 
+function Logo({ className }) {
+    const navigate = useNavigate();
+
+    const handleSeeMoreClick = () => {
+        navigate('/'); 
+    };
+    
     return (
-        <LogoImg 
+        <LogoImg
             className={className} // Passa 'className' para o elemento base
-            src={logo} 
-            alt='logo' 
+            src={logo}
+            alt='logo'
+            onClick={handleSeeMoreClick}
         />
     );
 }
